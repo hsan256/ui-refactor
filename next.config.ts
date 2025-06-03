690f72b3ae2598ea9766b076850f3d570e927253
hsan256 <hassan.trabelsi@arcube.org>
2025-06-03 23:41:15 +0100

    chore: add remote image pattern for Supabase integration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "yozbrydxdlcxghkphhtq.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
